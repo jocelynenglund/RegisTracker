412da7c665dcb3c9b6eca464307b9b0d5d483ec0
Jocelyn Englund <jocelyn@itsybit.se>
2025-01-11 11:46:41 +0100

    Initial tests implementation. Refactoring time next!

---
+++ RegisTrackerSystem/RegisTracker.cs
@@ -0,0 +1,34 @@
+﻿namespace RegisTrackerSystem;
+public record RegisterInterestCommand(string Email, int Year);
+public record RegistrationStatistics(int Total, IEnumerable<BatchStatistics> Batches);
+public record BatchStatistics(int Year, int Count);
+
+internal class RegisTracker(IRepository<Individual> individuals)
+{
+    public RegistrationStatistics GetStatistics()
+    {
+        return new RegistrationStatistics(0, new List<BatchStatistics>() { new BatchStatistics(2000, 1) });
+    }
+
+    public void Handle(RegisterInterestCommand interest)
+    {
+        if (individuals.GetAll().Any(x => x.Email == interest.Email))
+        {
+            throw new InvalidOperationException("That email is already registered");
+        }
+        individuals.Save(new Individual()
+        {
+            Email = interest.Email,
+        });
+    }
+}
+
+internal record Individual()
+{
+    public string Email { get; internal set; }
+}
+internal interface IRepository<T> where T : class
+{
+    void Save(T entity);
+    IEnumerable<T> GetAll();
+}
---
+++ RegisTrackerTests/RegistrationSystemTests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using RegisTrackerSystem;
+
+namespace RegisTrackerTests; 
+
+/// <summary>
+/// Test list:
+/// 
+/// When interest is registered, there should be 1 entry in the read model with a count of one 
+/// When an interest is registered for a specific year, the statistics should reflect that
+/// an email cannot be registered more than once
+/// </summary>
+/// 
+
+
+public class RegistrationSystemTests
+{
+    private readonly FakeRepository<Individual> fakeIndividualRepository = new();
+    private readonly RegisTracker sut;
+    public RegistrationSystemTests()
+    {
+        sut = new(fakeIndividualRepository);
+    }
+
+    [Fact]
+    public void WhenRegisterInterestCommandIsTriggeredOnce_StatisticsAreReflected()
+    {
+        var registerInterestCommand = new RegisterInterestCommand("aperson@example.com", 2000);
+
+        sut.Handle(registerInterestCommand);
+        var result = sut.GetStatistics();
+
+
+        result.Batches.Should().ContainSingle();
+        result.Batches.First(x=>x.Year == 2000).Count.Should().Be(1);
+    }
+
+    [Theory]
+    [InlineData(2)]
+    public void WhenRegisteringInterestMoreThanOnce_ThrowsException(int repetitions)
+    {
+        var registerInterestCommand = new RegisterInterestCommand("aperson@example.com", 2000);
+        sut.Handle(registerInterestCommand);
+        for (int i = 0; i < repetitions; i++)
+        {
+            Action act = () => sut.Handle(registerInterestCommand);
+            act.Should().Throw<InvalidOperationException>()
+                .WithMessage("That email is already registered",
+                because: "because repeat submissions are not allowed");
+        }
+    }
+
+}
+public class FakeRepository<T> : IRepository<T> where T : class
+{
+    public List<T> Entities { get; } = new();
+    public void Save(T entity)
+    {
+        Entities.Add(entity);
+    }
+    public IEnumerable<T> GetAll()
+    {
+        return Entities;
+    }
+}   
