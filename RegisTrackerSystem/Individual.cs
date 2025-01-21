0d9eb8c23ad6c828fb7899fe56533361ba4838e1
Jocelyn Englund <jocelyn@itsybit.se>
2025-01-21 22:58:28 +0100

    Building event sourced domain model.

--- RegisTrackerSystem/Individual.cs
+++ RegisTrackerSystem/Individual.cs
@@ -2,5 +2,54 @@
 
 internal record Individual()
 {
+    public Individual(List<DomainEvent> previousEvents) : this()
+    {
+        foreach (dynamic @event in previousEvents)
+        {
+            Apply(@event);
+        }
+    }
+
     public string Email { get; internal set; }
+    public Status Status { get; internal set; }
+
+    internal void ConfirmRegistration()
+    {
+        if (Status != Status.InterestSubmitted)
+        {
+            throw new InvalidOperationException(Status > Status.RegistrationConfirmed 
+                ? "Email already confirmed"
+                :"Email not registered");
+        }
+        Apply(new RegistrationConfirmed(Email));
+    }
+
+
+    internal void RegisterInterest(string email, int year)
+    {
+        Apply(new InterestRegistered(email, year));
+    }
+
+    private void Apply(InterestRegistered interestRegistered)
+    {
+        Email = interestRegistered.Email;
+        Status = Status.InterestSubmitted;
+    }
+    private void Apply(RegistrationConfirmed registrationConfirmed)
+    {
+        Status = Status.RegistrationConfirmed;
+    }
+
+}
+public abstract record DomainEvent()
+{
+    public DateTime TimeStamp { get; } = DateTime.UtcNow;
+}
+public record RegistrationConfirmed(string Email) : DomainEvent;
+public record InterestRegistered(string Email, int Year) : DomainEvent;
+public enum Status
+{
+    None,
+    InterestSubmitted,
+    RegistrationConfirmed
 }
---
+++ RegisTrackerTests/IndividualTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using RegisTrackerSystem;
+
+namespace RegisTrackerTests;
+public class IndividualTests
+{
+
+    [Fact]
+    public void WhenSubmittingInterest_SetsEmailAndStatus()
+    {
+        var individual = new Individual();
+        var cmd = new RegisterInterestCommand("a@person.com", 2000);
+
+        individual.RegisterInterest(cmd.Email, cmd.Year);
+
+        individual.Email.Should().Be(cmd.Email);
+        individual.Status.Should().Be(Status.InterestSubmitted);
+    }
+
+    [Fact]
+    public void WhenConfirmRegistration_ShouldChangeStatus()
+    {
+        var previousEvents = new List<DomainEvent>
+        {
+            new InterestRegistered("a@example.com", 2000)
+        };
+
+        var individual = new Individual(previousEvents);
+
+        individual.ConfirmRegistration();
+
+        individual.Status.Should().Be(Status.RegistrationConfirmed);
+    }
+
+    [Fact]
+    public void WhenConfirmRegistrationTwice_ShouldThrowException()
+    {
+        var previousEvents = new List<DomainEvent>
+        {
+            new InterestRegistered("a@example.com", 2000),
+            new RegistrationConfirmed("a@example.com")
+        };
+
+        var individual = new Individual(previousEvents);
+
+        Action act = () => individual.ConfirmRegistration();
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("Email already confirmed");
+
+    }
+
+    [Fact]
+    public void WhenConfirmRegistrationWithoutInterestSubmitted_ShouldThrowException()
+    {
+        var previousEvents = new List<DomainEvent>
+        {
+        
+        };
+
+        var individual = new Individual(previousEvents);
+
+        Action act = () => individual.ConfirmRegistration();
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("Email not registered");
+    }
+}
